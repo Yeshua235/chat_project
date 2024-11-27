be1c6495388250242bcbef10fae04cbc06eb05ea
Ayanlowo Joshua <joshuaayanlowo235@gmail.com>
2024-11-27 18:45:40 +0100

    still needs improvement

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,15 @@
-#from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import ChatRoom, Message
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
 
 class ChatRoomListView(LoginRequiredMixin, ListView):
     model = ChatRoom
